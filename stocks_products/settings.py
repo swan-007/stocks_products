28780f8e21da0b31b8019581ab3e26d67dce7688
Sergey Lebedev <superlebedew@gmail.com>
2023-06-07 19:36:28 +0300

    25

--- stocks_products/settings.py
+++ stocks_products/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
