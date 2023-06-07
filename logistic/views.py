152ac6fd7f56b8f396cc14a5c294acfc801fa4d0
Sergey Lebedev <superlebedew@gmail.com>
2023-06-07 17:30:59 +0300

    new2

--- logistic/views.py
+++ logistic/views.py
@@ -15,7 +15,6 @@ class ProductViewSet(ModelViewSet):
     search_fields = ['title', 'description']
 
 
-
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
